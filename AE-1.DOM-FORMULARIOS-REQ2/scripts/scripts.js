f2498d9b2340ee6f6d2a2c7ea981ece94cc65d70
Raul Garcia <rgarciat94@gmail.com>
2022-10-12 19:10:46 +0200

    Elimino required para probar js, pero no consigo que funciene XD

---
+++ AE-1.DOM-FORMULARIOS-REQ2/scripts/scripts.js
@@ -0,0 +1,36 @@
+//Constante nombre
+const nombre = document.getElementById("nombre");
+const telefono = document.getElementById("telefono");
+const direccion = document.getElementById("direccion");
+const email = document.getElementById("email");
+
+const formulario = document.getElementById("formulario");
+
+const pequenia = document.getElementById("pequenia")
+const mediana = document.getElementById("mediana")
+const grande = document.getElementById("grande")
+
+const ing1 = document.getElementById("ing1");
+const ing2 = document.getElementById("ing2");
+const ing3 = document.getElementById("ing3");
+const ing4 = document.getElementById("ing4");
+
+const btnEnviar = document.getElementById("btnEnviar");
+
+const parrafo = document.getElementById("warning");
+
+let validarFormulario = function(e){
+    let ok = true;
+    if(telefo.value == ""){
+        parrafo.innerHTML="Dato obligatorio";
+        ok = false;
+    }
+    return ok;
+}
+window.onload = function(){
+btnEnviar.onclick = function(e){
+    if(!validarFormulario()){
+        e.preventDefault();
+    }
+}
+}
