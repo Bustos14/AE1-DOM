15f28069b7bfd0b734077706d3998006d7f05d82
Álvaro Bustos González <alvaro9noviembre@gmail.com>
2022-10-16 16:21:02 +0200

    agregado 'formulario' de objetos perdidos

--- AE-1.DOM-FORMULARIOS-REQ1/js/eventos.js
+++ AE-1.DOM-FORMULARIOS-REQ1/js/eventos.js
@@ -1,13 +1,12 @@
 
 //Tabla, divFormulario como variable gobal
-let saltito = document.createElement("br");
+const saltito = document.createElement("br");
 function inicializador(){
     let tabla;
     tabla = document.createElement("table")
     cabeceraPagina(tabla);
     inicializadorSelect(tabla);
     inicializadorDivFormulario(tabla);
-
     divPrincipal.appendChild(tabla);
 }
 function cabeceraPagina(tabla){
@@ -19,7 +18,7 @@ function cabeceraPagina(tabla){
     mostrarLogo.setAttribute("heigth", "200");
     //Creo contenido para el titulo
     let contenedorTitulo = document.createElement("h1");
-    let contenidoTitulo = document.createTextNode("Bienvenido a la pagina de asistencia")
+    let contenidoTitulo = document.createTextNode("Bienvenido a la página de asistencia")
     contenedorTitulo.appendChild(contenidoTitulo);
 
     //Creo tabla contenedora de la cabecera
@@ -31,7 +30,7 @@ function cabeceraPagina(tabla){
     trCabecera.appendChild(th1)
     trCabecera.appendChild(th2)
     tabla.appendChild(trCabecera)
-    tabla.setAttribute("border", "1") //BORDE PROVISIONAL
+
     tabla.setAttribute("align", "center"); //CENTRADO PROVISIONAL
 }
 
@@ -100,6 +99,7 @@ function inicializadorDatos(tablaFormulario){
     trDatos.appendChild(saltito);
     trDatos.appendChild(lbTelefono);
     trDatos.appendChild(lbDireccion);
+    trDatos.setAttribute("style", "margin: 8px;")
     //agrego a tabla
     tablaFormulario.appendChild(trDatos);
    
@@ -107,31 +107,47 @@ function inicializadorDatos(tablaFormulario){
 
 //Funcion para inicializar y gestionar el dinamismo del formulario
 function formularioProblemas(tablaFormulario){
-    //Composición DNI
-    let lbDni = document.createElement("label");
-    let txtDni = document.createTextNode(" EJEMPLO PRUEBAS: ");
-    lbDni.appendChild(txtDni); 
-    let dni = document.createElement("input");
-    lbDni.appendChild(dni);
-    dni.setAttribute("type", "text");
-
+    let field = document.createElement("fieldset");
+    let titulo = document.createElement("legend");
+    let textTiulo = document.createTextNode("¿En que horario ocurrió? (Seleccione varios si hubo un cambio de horario en el transcurso del viaje)")
+    titulo.appendChild(textTiulo);
+    field.appendChild(titulo);
+    let posiblesObjetos = ["De madrugada(00:00-8:00)","Por la mañana(8:00-12:00)", "Tarde(12:00-20:00)", "Noche(20:00-00:00)"];
+    //Creamos radio button con 4 opciones
+    for(objetos of posiblesObjetos){    
+        const radio = document.createElement("label");
+        radio.textContent=objetos;
+        const radioBtn = document.createElement("input");
+        radioBtn.type="checkbox";
+        radioBtn.name="objeto";
+        radioBtn.id = objetos;
+        radioBtn.style.margin = "8px";
+        field.appendChild(radioBtn);
+        field.appendChild(radio);
+     
+    }
+    //Añadimos radioButtons a la tabla por medio de agregar el field a un nuevo tr
     let trDinamico = document.createElement("tr");
-    trDinamico.appendChild(lbDni);
+    trDinamico.appendChild(field);
+    trDinamico.id = "chilDinamico"
     tablaFormulario.appendChild(trDinamico);
     tablaFormulario.setAttribute("style", "height: 100%; width: 100%");
+    trDinamico.setAttribute("style", "height: 100%; width: 100%");
+    //Funcionalidad de radiobutton 
+    radioButton(trDinamico);
 
   
 }
 function formularioObjeto(tablaFormulario){
  
-    const field = document.createElement("fieldset");
-    const titulo = document.createElement("legend");
-    const textTiulo = document.createTextNode("¿Que tipo de objeto ha perdido?")
+    let field = document.createElement("fieldset");
+    let titulo = document.createElement("legend");
+    let textTiulo = document.createTextNode("¿Que tipo de objeto ha perdido?")
     titulo.appendChild(textTiulo);
     field.appendChild(titulo);
 
     let posiblesObjetos = ["Movil","Llaves", "Gafas", "Joyeria", "Otros"];
-    //Creamos radio button con 4 opciones
+    //Creamos radio button con 5 opciones
     for(objetos of posiblesObjetos){    
         const radio = document.createElement("label");
         radio.textContent=objetos;
@@ -140,15 +156,75 @@ function formularioObjeto(tablaFormulario){
         radioBtn.name="objeto";
         radioBtn.id = objetos;
         radioBtn.style.margin = "8px";
-        field.appendChild(radio);
         field.appendChild(radioBtn);
-
+        field.appendChild(radio);
     }
-  let trDinamico = document.createElement("tr");
-  trDinamico.appendChild(field);
-  trDinamico.id = "chilDinamico"
-  tablaFormulario.appendChild(trDinamico);
-  tablaFormulario.setAttribute("style", "height: 100%; width: 100%");
+    //Añadimos radioButtons a la tabla por medio de agregar el field a un nuevo tr
+    let trDinamico = document.createElement("tr");
+    trDinamico.appendChild(field);
+    trDinamico.id = "chilDinamico"
+    tablaFormulario.appendChild(trDinamico);
+    tablaFormulario.setAttribute("style", "height: 100%; width: 100%");
+    trDinamico.setAttribute("style", "height: 100%; width: 100%");
+    //Funcionalidad de radiobutton 
+    radioButton(trDinamico);
+    const divCheck = document.createElement("div");
+    divCheck.style.marginTop="20px";  
+        const checkbox = document.createElement("label");
+        checkbox.textContent="Sí";
+        const checkboxBtn= document.createElement("input");
+        checkboxBtn.type="checkbox";
+        checkboxBtn.id = "checkDescripcion"
+        checkboxBtn.style.margin = "8px";
+        field = document.createElement("fieldset");
+        titulo = document.createElement("legend");
+        textTiulo = document.createTextNode("¿Desea añadir una descripción detallada?")
+        titulo.appendChild(textTiulo);
+        field.appendChild(titulo);
+        field.appendChild(checkbox);
+        field.appendChild(checkboxBtn);
+        tablaFormulario.appendChild(divCheck);
+        divCheck.appendChild(field);
+        checkButton(field)
+        let btnEnviar = document.createElement("button");
+        let textBtn = document.createTextNode("Enviar")
+        btnEnviar.appendChild(textBtn);
+        tablaFormulario.appendChild(btnEnviar);
+        enviar(btnEnviar)
+}
+//Funcion que comprueba si el campo "otros" de los radioButton esta checkeado, haciendo aparecer o desaparecer
+// un area de texto
+function radioButton(trDinamico){
+    trDinamico.addEventListener("change", function(){
+        if(document.getElementById("Otros").checked){
+            let textInput = document.createElement("input");
+            textInput.placeholder = "Introduzca el tipo de objeto que ha perdido";
+            textInput.setAttribute("style", "heigh: 40%; width: 95%; margin: 8px")
+            trDinamico.appendChild(textInput);
+        }else{
+            vaciadoDinamico(trDinamico);
+        }
+    })
+        
+}
+function checkButton(divCheck){
+    divCheck.addEventListener("change", function(){
+        if(document.getElementById("checkDescripcion").checked){
+            let textAreaOpcion = document.createElement("textArea");
+            textAreaOpcion.placeholder = "Introduzca el tipo de objeto que ha perdido";
+            textAreaOpcion.setAttribute("style", "heigh: 40%; width: 95%; margin: 8px")
+            divCheck.appendChild(textAreaOpcion);
+        }else{
+            divCheck.childNodes[3].remove();
+        }
+    })
+        
+}
+function enviar(btnEnviar){
+    btnEnviar.addEventListener("click", function(){
+        document.write('<img src="./images/ASINSTANTE_free-file.png">');
+        document.write("<h1>¡Gracias! Contactaremos con usted lo antes posible.</h1>");
+    })
 }
 //Funcion para crear y gestionar el select de opciones
 function inicializadorSelect(tabla){
@@ -189,7 +265,6 @@ function interaccionEventoSelect(selects){
     }
 })
 }
-
 //Funcion que devuelve un array de objetos pelicula
 function listaOpcionesAyuda(){
     let ayudas = ["¿Que asistencia necesita?","Objeto perdido", "Problema con un conductor"];
@@ -201,7 +276,12 @@ function vaciadoDinamico(tabla){
     let nodoTable = tabla.childNodes
      //Comprobamos que si existe un elemento en la posicion 1 lo borre 
         //para asi eliminar el formulario si se ha creado
-        if(nodoTable[1]){
-            nodoTable[1].remove();
+        if(nodoTable.length > 1){
+            let i = nodoTable.length;
+            console.log(nodoTable.length)
+            while(nodoTable.length >1){
+                nodoTable[i-1].remove();
+                i--;
+            }
         }
 }
