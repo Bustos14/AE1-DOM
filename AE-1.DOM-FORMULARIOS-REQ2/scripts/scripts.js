2bd8d241ce4d31a2eb9a70efe2d7fd18f65f9d0c
Raul Garcia <rgarciat94@gmail.com>
2022-10-23 18:10:39 +0200

    Terminado

--- AE-1.DOM-FORMULARIOS-REQ2/scripts/scripts.js
+++ AE-1.DOM-FORMULARIOS-REQ2/scripts/scripts.js
@@ -12,31 +12,52 @@ let validarFormulario = function(e){
     return ok;
 }
 
+//función sumar
+function sumar(a=0,b=0){
+    pTotal.innerHTML = `El precio total es ${a + b} €`;
+}
+
 window.onload = function(){
 const btnEnviar = document.getElementById("btnEnviar");
 
 btnEnviar.onclick = function(e){
     
+    if(!validarFormulario()){
+        e.preventDefault();
+    }
+}
+
+btnEnviar.onclick = function(){
+    
     //Variable para seleccionar los radios con el nombre tamaño y esten chekeados
     let radioActivo = document.querySelector('input[name="tamaño"]:checked');
     //Variable para seleccionar los check con el nombre ingredientes
-    let checks = document.querySelector('input[name="ingredientes"]:checked');
+    let checks = document.querySelectorAll('input[type="checkbox"]:checked');
+    //Creo una variable que me dirá la cantidad total de checkbox checkeados
+    let cont = 0; 
+    //Creo variable para seleccionar el p con el id = total
     let pTotal = document.getElementById("total");
     let total = 0;
-    if(!validarFormulario()){
-        e.preventDefault();
-    }if(radioActivo) {
+    //Si hay algun radio activo
+    if(radioActivo) {
+        //a total le sumamos el valor del radio
         total += Number(radioActivo.value);
-    }if(checks) {
-        total += Number(checks.value);
-    }
-    
-    else {
+        //Recorremos con un for la lista de los checks marcados, por cada check marcado
+        //le sumamos 1 (de 1€) por ingrediente
+        for(let i = 0; i < checks.length; i++){
+            if(checks[i].checked){
+                cont = cont + 1;
+            }
+        }
+        //Su mamos al total la cantidad de checks marcados
+        total = total + cont;
+        //En caso de que no haya ningun radio marcado lanzamos el mensaje mediante alert
+    }else {
         e.preventDefault();
         alert('Debes seleccionar un tamaño y almenos un ingrediente');
     }
 
-
+    //Por último modificamos el HTML del <p> con id total y le introducimos el total de la pizza
     pTotal.innerHTML = `El precio total es ${total} €`
 
 }
